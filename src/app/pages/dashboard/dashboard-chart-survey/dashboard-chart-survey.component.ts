3b2eb0af44dd75b3391f7ae2b93d3af4418067a2
Niseel <nguyencongthanh200199@gmail.com>
2022-06-15 15:31:53 +0700

    [Debug] Resolve Conflict 2

--- src/app/pages/dashboard/dashboard-chart-survey/dashboard-chart-survey.component.ts
+++ src/app/pages/dashboard/dashboard-chart-survey/dashboard-chart-survey.component.ts
@@ -14,11 +14,7 @@ Chart.register(...registerables);
 export class DashboardChartSurveyComponent implements OnInit, AfterViewInit {
   private chartInstance?: Chart;
 
-<<<<<<< HEAD
   public chartTitle = '';
-=======
-  public chartTitle = 'Conflict nè';
->>>>>>> 931bce137622ae8845e3be7bddf5b0990b51cead
 
   constructor() {}
 
